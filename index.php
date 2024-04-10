7fa312733d9899a9a5159c80b56d9c72271b3513
Charlie Boardman <cboardman2@gmail.com>
2024-04-10 00:20:37 -0700

    Added functionality to create new transaction line to apend to transactions.csv

--- index.php
+++ index.php
@@ -9,6 +9,28 @@
 <!--Check when app was last opened and add number of days needed-->
 <?php
 
+#Functions
+function transact($date,$amount,$type,$vendor,$description) {
+    
+    $old_total = floatval(file_get_contents('total.txt'));
+
+    if ($type == 'gain') {
+        $new_total = $old_total + $amount;
+    }
+    
+    if ($type == 'spend') {
+        $new_total = $old_total - $amount;
+    }
+
+    $date_str = $date->format('Y-m-d');
+
+    $new_transaction = "$date_str,$amount,$type,$vendor,$description,$new_total\n";
+
+    return $new_transaction;
+}
+
+#Begin main logic
+
 #Set the timezone
 date_default_timezone_set('America/Los_Angeles');
 
@@ -23,9 +45,12 @@ $days_since_opening = date_diff($last_opened,$today,TRUE);
 
 if ($days_since_opening != 0) {
     #Get daily increment
-    $daily_increment = file_get_contents('daily-increment.txt'); #Need to write this function
-    increment($days_since_opening,$daily_increment);
+    $daily_increment = file_get_contents('daily-increment.txt');
+    $days_since_opening_int = $days_since_opening->format('%a');
+    $new_line = transact($today,$daily_increment * $days_since_opening_int,'gain','',"Daily increment ($days_since_opening_int)");
+    file_put_contents('transactions.csv',$new_line, FILE_APPEND);
 }
+
 
 
 echo $today->format('Y-m-d');
@@ -35,4 +60,6 @@ echo "<br>";
 echo $days_since_opening->format('%a');
 echo "<br>";
 echo $daily_increment;
+echo "<br>";
+echo $new_line;
 ?>
